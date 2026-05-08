e073a07b54287bab3d4aafcb173ce8457abf6028
Joudalshehri <jshehri0001@stu.kau.edu.sa>
2026-05-08 18:44:59 +0300

    Remove duplicated nested folders

--- backend/frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/App";
-import "./styles/index.css";
-import "./styles/styles.css";
-
-const root = document.getElementById("root");
-
-createRoot(root).render(<App />);
